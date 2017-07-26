41f195b7c1eed472480148e872e11516052329ed
gumanx <gumanx0@gmail.com>
2017-07-26 14:45:53 -0400

    Coins no longer overriden after each game. Added prices underneath dots in customize screen and made them one-time purchases

--- app/src/main/java/com/gumanx/dotdodger/CustomizeActivity.java
+++ app/src/main/java/com/gumanx/dotdodger/CustomizeActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 public class CustomizeActivity extends AppCompatActivity {
 
     private int coins;
+    private SharedPreferences prefs;
     private SharedPreferences.Editor editor;
 
     @Override
@@ -19,7 +20,7 @@ public class CustomizeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_customize);
 
         // Loads stored data
-        SharedPreferences prefs = getSharedPreferences("dotDodgePrefs", Context.MODE_PRIVATE);
+        prefs = getSharedPreferences("dotDodgePrefs", Context.MODE_PRIVATE);
         editor = prefs.edit();
         coins = prefs.getInt("coins", 0);
 
@@ -27,24 +28,28 @@ public class CustomizeActivity extends AppCompatActivity {
     }
 
     // Subtracts cost of dot from stored coins and changes the dot color
-    protected void setDot(int color, int cost) {
-        if (cost <= coins) {
+    private void setDot(int color, int cost) {
+        if (!prefs.getBoolean("bought" + color, false) && cost <= coins) {
             coins = coins - cost;
             editor.putInt("coins", coins);
-            editor.putInt("dotColor", color);
-            editor.apply();
+            editor.putBoolean("bought" + color, true);
             updateCoins();
         }
+        if (prefs.getBoolean("bought" + color, false)) {
+            editor.putInt("dotColor", color);
+        }
+        editor.apply();
     }
 
-    public void selectDefault(View view) {
+    public void selectCyan(View view) {
         setDot(Color.CYAN, 0);
     }
 
     public void selectWhite(View view) {
-        setDot(Color.WHITE, 10);
+        setDot(Color.WHITE, 100);
     }
 
+    // Refreshes the coin display
     private void updateCoins() {
         TextView coinText = (TextView) findViewById(R.id.coinText);
         coinText.setText(Integer.toString(coins));
--- app/src/main/java/com/gumanx/dotdodger/GameOverActivity.java
+++ app/src/main/java/com/gumanx/dotdodger/GameOverActivity.java
@@ -20,17 +20,20 @@ public class GameOverActivity extends AppCompatActivity {
         TextView scoreIntText = (TextView) findViewById(R.id.scoreIntText);
         scoreIntText.setText(Integer.toString(score));
 
-        // Compares score against stored high score
-        // If the score is larger, set it as the new high score
         SharedPreferences prefs = getSharedPreferences("dotDodgePrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor prefsEditor = prefs.edit();
-        prefsEditor.putInt("coins", score);
+
+        // Compares score against stored high score
+        // If the score is larger, set it as the new high score
         if (prefs.getInt("highScore", 0) < score) {
             TextView scoreText = (TextView) findViewById(R.id.scoreText);
             scoreText.setText("New High Score:");
 
             prefsEditor.putInt("highScore", score);
         }
+
+        // Adds coins and applies all changes
+        prefsEditor.putInt("coins", prefs.getInt("coins", 0) + score);
         prefsEditor.apply();
     }
 
